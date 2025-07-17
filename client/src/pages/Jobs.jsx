54243d348124857c41cd88b1dbea486aa984dc9e
SimonNungwa <simonnungwa99@gmail.com>
2025-07-17 23:09:17 +0100

    Added Jobs page

---
+++ client/src/pages/Jobs.jsx
@@ -0,0 +1,11 @@
+const Jobs = () => {
+    return (
+      <div className="min-h-screen bg-white text-black py-10 px-4">
+        <h1 className="text-4xl font-bold mb-6 text-center">Job Listings</h1>
+        <p className="text-center text-gray-600">Browse all available jobs here.</p>
+      </div>
+    );
+  };
+  
+  export default Jobs;
+  
