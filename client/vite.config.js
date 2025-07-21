5002beb3513da56480e63fe7aeddf650e7c47ef0
SimonNungwa <simonnungwa99@gmail.com>
2025-07-21 20:34:05 +0100

    updated config: Added server config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),    
+    tailwindcss(),
   ],
   base: process.env.VITE_BASE_PATH || "/",
-})
 
+  // 👇 Add this server config
+  server: {
+    host: true,          // Equivalent to 0.0.0.0
+    port: 5173,          // Optional: can be changed
+    strictPort: true     // Fail if port is in use
+  }
+})
